d396f0a4b2ef1827239b9ac909a8a9e8b73b748c
seunghun-5945 <tmdgns5945@oasis.inje.ac.kr>
2025-03-31 23:01:16 +0900

    feat: post upload

--- src/app/posts/[id]/page.js
+++ src/app/posts/[id]/page.js
@@ -38,34 +38,43 @@ export default async function Post({ params }) {
   const postData = await getPostData(id);
 
   return (
-    <div className="w-full py-6 px-4">
+    <div className="w-full py-6 px-4 flex flex-col items-center">
+      {/* 포스트 제목 */}
       <h1 className="w-full text-4xl text-center font-bold mb-4 text-black">
         {postData.title}
       </h1>
-      <hr />
 
-      <div className="w-full max-w-[50vw] mx-auto px-20">
-        <div className="flex mb-4 py-4 text-black">
-          <div className="w-1/2">
+      {/* 포스트 박스 전체 */}
+      <div className="flex flex-col items-center w-full">
+        {/* 작성일 작성자 공유하기 메뉴바 */}
+        <div className="w-2/5 flex justify-between text-black">
+          <div className="w-1/2 flex justify-start text-md">
+            {/* 날짜 */}
             <span>{postData.date}</span>
+            {/* 작성자 */}
             <span>{postData.author}</span>
+            {/* 카테고리 */}
             <span>{postData.category}</span>
           </div>
-          <div className="w-1/2 flex justify-end text-2xl">
+
+          {/* 공유하기 아이콘 바 */}
+          <div className="w-1/2 flex justify-end items-center text-2xl">
             <FaLink className="mr-3" />
             <RiKakaoTalkFill />
           </div>
         </div>
 
+        <hr className="w-full border-t-2 border-gray-300 my-4" />
+
         {/* Markdown 변환 */}
-        <div className="prose">
+        <div className="flex flex-col w-2/3 py-10 mx-auto text-black items-center prose">
           <ReactMarkdown rehypePlugins={[rehypeRaw]}>
             {postData.content}
           </ReactMarkdown>
         </div>
       </div>
 
-      <div className="pt-20">
+      <div className="w-full">
         <Disqus post={postData} />
       </div>
     </div>
